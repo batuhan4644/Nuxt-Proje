f767a5b9f337179eb88d9ea7b019d6f83f1734c8
batuhan görgel <batuhan4644@gmail.com>
2020-12-07 01:15:33 +0300

    Update IslemBasarili.vue

--- pages/IslemBasarili.vue
+++ pages/IslemBasarili.vue
@@ -14,3 +14,43 @@ export default {
   
 };
 </script>
+<style>
+.secenek {
+  cursor: pointer;
+  background-color: lightgray;
+  padding: 7px;
+ width: 90%;
+  color: black;
+  font-size: 12px;
+  text-align: center;
+}
+.secilmis {
+  background-color: gray;
+  color: white;
+}
+.mega-menu .dropdown {
+    position: static;
+}
+
+ .mega-menu .dropdown-menu {
+    border-top: 0;
+    border-radius: 3px;
+    background-color: #fff;
+    width: 100%;
+    left:0;
+    right:0;
+    top:47px;
+    position: absolute;
+    -webkit-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.3);
+    -moz-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.3);
+    box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.3);
+}
+    
+.mega-menu .dropdown:hover .dropdown-menu, 
+ .mega-menu .dropdown .dropdown-menu:hover {
+    display:block!important;
+ }
+ 
+
+
+</style>
